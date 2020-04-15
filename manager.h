4025b5eef0dbc0b127714c95adba806e9ba4ca7b
Taeyoung Lee <taeyounglee19@gmail.com>
2020-04-16 00:59:00 +0900

    C1- manager.h - add loadfile func

---
+++ manager.h
@@ -0,0 +1,10 @@
+//mini_project
+//manager.h
+//loadfile function added
+#include <stdio.h>
+#include <string.h>
+#include "product.h"
+
+void listproduct(PRODUCT* p, int count);    //제품 전체 목록 조회
+int select_product_no(PRODUCT* p, int count);   //제품 번호 선택
+int loadfile(PRODUCT* p);   //파일 불러오기
