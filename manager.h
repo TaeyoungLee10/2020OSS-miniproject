b1e86e6138d100144750897e93cc4132e9b92dbf
Taeyoung Lee <taeyounglee19@gmail.com>
2020-04-16 01:00:26 +0900

    C2- manager.h - add savefile func

--- manager.h
+++ manager.h
@@ -1,6 +1,7 @@
 //mini_project
 //manager.h
 //loadfile function added
+//savefile function added
 #include <stdio.h>
 #include <string.h>
 #include "product.h"
@@ -8,3 +9,4 @@
 void listproduct(PRODUCT* p, int count);    //제품 전체 목록 조회
 int select_product_no(PRODUCT* p, int count);   //제품 번호 선택
 int loadfile(PRODUCT* p);   //파일 불러오기
+void savefile(PRODUCT* p, int count);   //파일 저장하기
