1fb3be2f311552187d161a0753a34255a0856b2d
Taeyoung Lee <taeyounglee19@gmail.com>
2020-04-16 01:02:13 +0900

    C3- manager.h - add search_product func

--- manager.h
+++ manager.h
@@ -2,6 +2,7 @@
 //manager.h
 //loadfile function added
 //savefile function added
+//search_product function added
 #include <stdio.h>
 #include <string.h>
 #include "product.h"
@@ -10,3 +11,4 @@ void listproduct(PRODUCT* p, int count);    //제품 전체 목록 조회
 int select_product_no(PRODUCT* p, int count);   //제품 번호 선택
 int loadfile(PRODUCT* p);   //파일 불러오기
 void savefile(PRODUCT* p, int count);   //파일 저장하기
+void searchproduct(PRODUCT* p, int count);   //제품 검색
